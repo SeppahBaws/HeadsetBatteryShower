357292e55f959a41ba983b934cb5e5e07da9733c
Seppe Dekeyser <seppe@dva.be>
2022-03-02 22:31:26 +0100

    Read the iCue logs directory from a config file

--- HeadsetBatteryShower/HeadsetBatteryShower.cpp
+++ HeadsetBatteryShower/HeadsetBatteryShower.cpp
@@ -3,89 +3,121 @@
 #include <filesystem>
 #include <regex>
 
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+
 using namespace std::literals;
+using namespace std::string_literals;
 namespace fs = std::filesystem;
 
-void PrintVector(const std::vector<std::string>& vec)
+std::string GetExePath()
 {
-	for (auto it = vec.cbegin(); it != vec.cend(); it++)
-	{
-		std::cout << *it << std::endl;
-	}
+	char result[MAX_PATH]{};
+	const std::string path = std::string(result, GetModuleFileNameA(nullptr, result, MAX_PATH));
+
+	const size_t pos = path.find_last_of("/\\");
+	return path.substr(0, pos + 1);
 }
 
 int main()
 {
-	const std::string directory = "C:/Users/seppe/AppData/Local/Corsair/CUE4/logs";
-	std::vector<fs::directory_entry> files;
-
-	// Get all .log files in directory.
-	for (const auto entry : fs::directory_iterator(directory))
+	try
 	{
-		if (entry.is_regular_file() && entry.path().extension() == ".log")
+		std::string iCueDir;
+
+		// Read iCue directory from config file.
 		{
-			files.push_back(entry);
-		}
-	}
-
-	if (files.size() == 0)
-	{
-		throw std::exception("No .log files present in logs folder");
-	}
-
-	// Sort files based on last write time.
-	std::sort(files.begin(), files.end(), [](const fs::directory_entry& left, const fs::directory_entry& right)
-	{
-		return left.last_write_time() > right.last_write_time();
-	});
-
-	const fs::path path = files[0].path();
-	std::ifstream file(path);
-	if (!file.is_open())
-		throw std::exception(("Failed to open file \""s + path.string() + "\"").c_str());
-
-	std::vector<std::string> entries;
-	std::string t;
-	const std::string entryKey = "cue.hid_dev_battery_controller: Battery data from";
-	while (std::getline(file, t))
-	{
-		if (t.find(entryKey) != std::string::npos)
-		{
-			entries.push_back(t);
-		}
-	}
-
-	// Make sure we close the file, so that the iCue app can write to it.
-	file.close();
-
-
-
-	std::cout << "Found " << entries.size() << " battery level entries:" << std::endl;
-
-	//PrintVector(entries);
-
-	std::regex r("^(\\d{4}-\\d{2}-\\d{2}\\s\\d{2}:\\d{2}:\\d{2}.\\d{3}).*cue.hid_dev_battery_controller: Battery data from \".*\"\\s(\\d+).*$");
-	for (auto it = entries.begin(); it != entries.end(); it++)
-	{
-		auto b = std::sregex_iterator((*it).begin(), (*it).end(), r);
-		auto e = std::sregex_iterator();
-
-		for (std::sregex_iterator i = b; i != e; i++)
-		{
-			std::smatch match = *i;
-			std::string date, battery;
-			if (match.size() == 3)
+			const std::string configPath = GetExePath() + "iCueDir.txt";
+			std::ifstream file(configPath, std::ios::binary);
+			if (!file)
 			{
-				date = match[1];
-				battery = match[2];
+				throw std::runtime_error("Unable to open config file: '"s + configPath + "'"s);
+			}
 
-				std::cout << "date: " << date << " -- battery: " << battery << std::endl;
+			file.seekg(0, std::ios::beg);
+			std::getline(file, iCueDir);
+			file.close();
+
+			if (iCueDir.empty())
+			{
+				throw std::runtime_error("iCue directory could not be read!");
 			}
 		}
 
-		std::cout << std::endl;
-	}
+		std::vector<fs::directory_entry> files;
 
-	std::cout << "Press Enter to quit" << std::endl;
-	std::cin.get();
+		// Get all .log files in directory.
+		for (const auto& entry : fs::directory_iterator(iCueDir))
+		{
+			if (entry.is_regular_file() && entry.path().extension() == ".log")
+			{
+				files.push_back(entry);
+			}
+		}
+
+		if (files.empty())
+		{
+			throw std::runtime_error("No .log files present in logs folder");
+		}
+
+		// Sort files based on last write time.
+		std::ranges::sort(files, [](const fs::directory_entry& left, const fs::directory_entry& right)
+			{
+				return left.last_write_time() > right.last_write_time();
+			});
+
+		const fs::path path = files[0].path();
+		std::ifstream file(path);
+		if (!file.is_open())
+			throw std::runtime_error(("Failed to open file \""s + path.string() + "\"").c_str());
+
+		std::vector<std::string> entries;
+		std::string t;
+		const std::string entryKey = "cue.hid_dev_battery_controller: Battery data from";
+		while (std::getline(file, t))
+		{
+			if (t.find(entryKey) != std::string::npos)
+			{
+				entries.push_back(t);
+			}
+		}
+
+		// Make sure we close the file, so that the iCue app can write to it.
+		file.close();
+
+
+
+		std::cout << "Found " << entries.size() << " battery level entries:" << std::endl;
+
+		std::regex r(
+			R"(^(\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2}.\d{3}).*cue.hid_dev_battery_controller: Battery data from ".*"\s(\d+).*$)");
+		for (auto& entry : entries)
+		{
+			auto b = std::sregex_iterator(entry.begin(), entry.end(), r);
+			auto e = std::sregex_iterator();
+
+			for (std::sregex_iterator i = b; i != e; ++i)
+			{
+				std::smatch match = *i;
+				if (match.size() == 3)
+				{
+					std::string battery;
+					std::string date;
+					date = match[1];
+					battery = match[2];
+
+					std::cout << "date: " << date << " -- battery: " << battery << std::endl;
+				}
+			}
+
+			std::cout << std::endl;
+		}
+
+		std::cout << "Press Enter to quit" << std::endl;
+		std::cin.get();
+	}
+	catch (std::runtime_error& e)
+	{
+		std::cout << "ERROR: " << e.what() << std::endl;
+	}
 }
---
+++ README.md
@@ -0,0 +1,24 @@
+# Headset Battery Shower
+
+Have a Corsair Headset? Can't see the exact battery level?
+
+Welp this is the solution for you!
+
+# How to use
+
+1. Download and extract the files from the releases tab.
+2. Set your username on this PC in the `iCueDir.txt` file
+3. Double-click the executable.
+
+Now you see an extract of all log entries which mention "battery" and have a number in it.
+The last one will be your headset's latest recorded battery level.
+
+# Building the source code
+
+This project runs on the C++20 standard.
+1. Open the project in Visual Studio 2022 (2019 might not work correctly, because the project requires C++20 and MSVC v143)
+2. Build and run.
+
+---
+
+ps: Corsair please fix your iCue software, this is unacceptable.
